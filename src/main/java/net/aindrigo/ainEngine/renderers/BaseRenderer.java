810d92fd359f55783d8ddeb934e872b203d99345
aindrigo <adamindrigo@gmail.com>
2021-01-29 09:17:28 -0500

    Simple blue screen, nothing advanced.

--- src/main/java/net/aindrigo/ainEngine/renderers/BaseRenderer.java
+++ src/main/java/net/aindrigo/ainEngine/renderers/BaseRenderer.java
@@ -1,11 +1,16 @@
 package net.aindrigo.ainEngine.renderers;
 
+import net.aindrigo.ainEngine.main.Engine;
+/**
+ * @author aindrigo
+ */
 public class BaseRenderer {
-    public BaseRenderer(){
-
+    public Engine e;
+    public BaseRenderer(Engine e){
+        this.e = e;
+        e.renderManager.renderers.add(this);
     }
 
-    public void update(){
-
-    }
+    public void update(){};
+    public void start(){};
 }
---
+++ src/main/java/net/aindrigo/ainEngine/test/TESTCLASS.java
@@ -0,0 +1,9 @@
+package net.aindrigo.ainEngine.test;
+
+public class TESTCLASS {
+    public static void main(String[] args){
+        Tester tester = new Tester();
+        System.out.println("Tester working");
+        tester.test();
+    }
+}
